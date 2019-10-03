92db9b78a7ac97e7d2b6bf67d82bbe357eba2a5e
Zura Sekhniashvili <zurasekhniashvili@gmail.com>
2019-10-03 09:17:48 +0400

    Remove fixed navbar, remove php requirements

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -32,7 +32,7 @@ AppAsset::register($this);
         'brandLabel' => Yii::$app->name,
         'brandUrl' => Yii::$app->homeUrl,
         'options' => [
-            'class' => 'navbar-inverse navbar-fixed-top',
+            'class' => 'navbar-inverse',
         ],
     ]);
     echo Nav::widget([
