1c760df1e818b6aa07021c71cb705755ba57523c
Zura Sekhniashvili <zurasekhniashvili@gmail.com>
2019-10-23 08:54:26 +0400

    Demonstrate aliases

--- web/index.php
+++ web/index.php
@@ -10,3 +10,12 @@ require __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
 $config = require __DIR__ . '/../config/web.php';
 
 (new yii\web\Application($config))->run();
+
+Yii::setAlias('@home', '/home/thecodeholic/public_html');
+Yii::setAlias('@domain', '@home/http://yii2-basic.test');
+//Yii::setAlias('@test', '/something/test');
+//Yii::setAlias('@test/test2', '/something/test/test2');
+//Yii::setAlias('@test/test2.php', '/something/test/test2.php');
+//
+//Yii::getAlias('@home/something'); // /home/thecodeholic/public_html/something
+//Yii::getAlias('@test/test2');
